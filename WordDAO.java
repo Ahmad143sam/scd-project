d7d20d03bee0205640ae61bc0cb575315a768d8e
Fida Kainth <145021745+Fida-Kainth@users.noreply.github.com>
2024-10-11 18:10:42 +0500

    Update WordDAO.java
    Adding data access object for CRUD operations

--- WordDAO.java
+++ WordDAO.java
@@ -25,6 +25,44 @@ public class WordDAO {
         }
         return words;
     }
+    // Add a new word to CSV file
+    public void addWord(WordDTO word) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath, true))) {
+            bw.write(word.getUrduWord() + "," + word.getPersianMeaning() + "," + word.getArabicMeaning() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Update an existing word in CSV
+    public void updateWord(WordDTO word) {
+        List<WordDTO> words = getAllWords();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            for (WordDTO w : words) {
+                if (w.getUrduWord().equals(word.getUrduWord())) {
+                    bw.write(word.getUrduWord() + "," + word.getPersianMeaning() + "," + word.getArabicMeaning() + "\n");
+                } else {
+                    bw.write(w.getUrduWord() + "," + w.getPersianMeaning() + "," + w.getArabicMeaning() + "\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Delete a word from CSV file
+    public void deleteWord(String urduWord) {
+        List<WordDTO> words = getAllWords();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            for (WordDTO w : words) {
+                if (!w.getUrduWord().equals(urduWord)) {
+                    bw.write(w.getUrduWord() + "," + w.getPersianMeaning() + "," + w.getArabicMeaning() + "\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     
 }
