b6b056d184af68a0b54006b171c119005aa6fb32
Fida Kainth <145021745+Fida-Kainth@users.noreply.github.com>
2024-10-11 18:07:24 +0500

    Update DictionaryTablePanel.java
    setting up jtable for the crud data operations

--- DictionaryTablePanel.java
+++ DictionaryTablePanel.java
@@ -37,7 +37,77 @@ public class DictionaryTablePanel extends JPanel {
             data[i][2] = word.getArabicMeaning();
         }
     }
+  // Buttons for Add, Update, Delete
+        JPanel buttonPanel = new JPanel();
+        JButton addButton = new JButton("Add Word");
+        JButton updateButton = new JButton("Update Word");
+        JButton deleteButton = new JButton("Delete Word");
 
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Logic to add a new word
+                manager.addWordDialog();
+                refreshTable(); // Refresh the table after adding
+            }
+        });
+
+        updateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Logic to update the selected word
+                int selectedRow = table.getSelectedRow();
+                if (selectedRow != -1) {
+                    String urduWord = (String) table.getValueAt(selectedRow, 0);
+                    manager.updateWordDialog(urduWord);
+                    refreshTable(); // Refresh the table after updating
+                } else {
+                    showMessage("Please select a word to update.");
+                }
+            }
+        });
+
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Logic to delete the selected word
+                int selectedRow = table.getSelectedRow();
+                if (selectedRow != -1) {
+                    String urduWord = (String) table.getValueAt(selectedRow, 0);
+                    int confirm = JOptionPane.showConfirmDialog(
+                            null,
+                            "Are you sure you want to delete " + urduWord + "?",
+                            "Confirm Delete",
+                            JOptionPane.YES_NO_OPTION
+                    );
+                    if (confirm == JOptionPane.YES_OPTION) {
+                        manager.deleteWord(urduWord);
+                        refreshTable(); // Refresh the table after deletion
+                    }
+                } else {
+                    showMessage("Please select a word to delete.");
+                }
+            }
+        });
+
+        buttonPanel.add(addButton);
+        buttonPanel.add(updateButton);
+        buttonPanel.add(deleteButton);
+
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    // Load data from the manager into the table
+    private void loadData() {
+        List<WordDTO> words = manager.getAllWords();
+        data = new String[words.size()][3];
+        for (int i = 0; i < words.size(); i++) {
+            WordDTO word = words.get(i);
+            data[i][0] = word.getUrduWord();
+            data[i][1] = word.getPersianMeaning();
+            data[i][2] = word.getArabicMeaning();
+        }
+    }
    
     private void refreshTable() {
         loadData(); 
